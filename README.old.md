040c08475cf5b1e56ae947c163c3d700f9f0ca64
Johansen512 <tekstogtext@gmail.com>
2020-11-12 13:42:44 +0100

    noget med Forms

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# erdettidnu
---
+++ src/Components/Dateform.js
@@ -0,0 +1,30 @@
+/**@jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+const Dateform= () => {
+
+   
+    const style = css`
+    border: 40px solid red;
+    width: 400px;
+    height: 400px;
+    background-color:blue; 
+    
+    `;
+    return (
+       
+/*<h1>Haaaaalloooooooo</h1>
+        <form >
+  <label>
+    Name:
+    <input type="date" name="Dato" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>*/
+
+
+
+      );
+}
+ 
+export default Dateform;
